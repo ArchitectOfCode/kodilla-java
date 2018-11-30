fbc756c6e3c99d4c9002c46499c001fb63ae2089
ArchitectOfCode <zbigniew_g@o2.pl>
2018-11-30 23:07:35 +0100

    Exercise 7.5: Functional calculation of the average

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -4,8 +4,20 @@ import java.util.stream.IntStream;
 
 public interface ArrayOperations {
     public static double getAverage(int[] numbers) {
+        /*
         IntStream.of(numbers)
                 .forEach(s -> System.out.println(s));
+        */
+        IntStream.range(0, numbers.length)
+                .map(n -> numbers[n])
+                .forEach(s -> System.out.println(s));
+
+        /*
         return IntStream.of(numbers).average().getAsDouble();
+        */
+        return IntStream.range(0, numbers.length)
+                .map(n -> numbers[n])
+                .average()
+                .getAsDouble();
     }
 }

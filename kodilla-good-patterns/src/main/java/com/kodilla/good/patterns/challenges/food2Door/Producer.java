83db12b603b7b8c14fcaefcf8768c5c214941207
ArchitectOfCode <zbigniew_g@o2.pl>
2018-12-27 13:09:07 +0100

    Exercise 9.3: Food2Door

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/food2Door/Producer.java
@@ -0,0 +1,8 @@
+package com.kodilla.good.patterns.challenges.food2Door;
+
+public interface Producer {
+    public String getName();
+    public String getProduct();
+    public double getQuantity();
+    public boolean process();
+}

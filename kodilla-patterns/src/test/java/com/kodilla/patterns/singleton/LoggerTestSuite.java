9971a822ad018420fc176e0f5d26add0a31b8779
ArchitectOfCode <zbigniew_g@o2.pl>
2019-01-09 00:50:12 +0100

    Exercise 11.1: Singleton for saving logs of performed operations

--- kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -5,12 +5,23 @@ import org.junit.Test;
 
 public class LoggerTestSuite {
     @Test
-    public void testGetFileName() {
+    public void testAddOneEvent() {
         //Given
         //When
         Logger.getInstance().log("test event");
         String messageFromLog = Logger.getInstance().getLastLog();
         //Then
         Assert.assertEquals("test event", messageFromLog);
+    }
+
+    @Test
+    public void testAddTwoEvents() {
+        //Given
+        //When
+        Logger.getInstance().log("test event");
+        Logger.getInstance().log("another test event");
+        String messageFromLog = Logger.getInstance().getLastLog();
+        //Then
+        Assert.assertEquals("another test event", messageFromLog);
     }
 }

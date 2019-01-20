b45a4656eb65bb877f3c426da034a07f54343085
ArchitectOfCode <zbigniew_g@o2.pl>
2019-01-20 23:05:59 +0100

    Module 13.3: Creating relations between tables

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Company.java
@@ -0,0 +1,54 @@
+package com.kodilla.hibernate.manytomany;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "companies")
+public class Company {
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "id", unique = true)
+    private int id;
+
+    @NotNull
+    @Column(name = "company_name")
+    private String name;
+
+    @ManyToMany(cascade = CascadeType.ALL, mappedBy = "companies")
+    private List<Employee> employees = new ArrayList<>();
+
+    public Company() {
+    }
+
+    public Company(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+}
--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/task/dao/TaskDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/task/dao/TaskDaoTestSuite.java
@@ -1,15 +1,21 @@
 package com.kodilla.hibernate.task.dao;
 
 import com.kodilla.hibernate.task.Task;
+import com.kodilla.hibernate.task.TaskFinancialDetails;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class);
+import java.math.BigDecimal;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class TaskDaoTestSuite {
+    @Autowired
     private TaskDao taskDao;
     private static final String DESCRIPTION = "Test: Learn Hibernate";
 
@@ -25,6 +31,41 @@ public class TaskDaoTestSuite {
         int id = task.getId();
         Task readTask = taskDao.findOne(id);
         Assert.assertEquals(id, readTask.getId());
+
+        //CleanUp
+        taskDao.delete(id);
+    }
+
+    @Test
+    public void testTaskDaoFindByDuration() {
+        //Given
+        Task task = new Task(DESCRIPTION, 7);
+        taskDao.save(task);
+        int duration = task.getDuration();
+
+        //When
+        List<Task> readTasks = taskDao.findByDuration(duration);
+
+        //Then
+        Assert.assertEquals(1, readTasks.size());
+
+        //CleanUp
+        int id = readTasks.get(0).getId();
+        taskDao.delete(id);
+    }
+
+    @Test
+    public void testTaskDaoSaveWithFinancialDetails() {
+        //Given
+        Task task = new Task(DESCRIPTION, 30);
+        task.setTaskFinancialDetails(new TaskFinancialDetails(new BigDecimal(120), false));
+
+        //When
+        taskDao.save(task);
+        int id = task.getId();
+
+        //Then
+        Assert.assertNotEquals(0, id);
 
         //CleanUp
         taskDao.delete(id);

d43dd93c18c59d6ca73b6b2db492084dee96703c
ArchitectOfCode <zbigniew_g@o2.pl>
2019-01-27 22:55:07 +0100

    Exercise 13.4: Searching workers and companies (HQL and Named Queries)

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
@@ -9,11 +9,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CompanyDaoTestSuite {
     @Autowired
     CompanyDao companyDao;
+    @Autowired
+    EmployeeDao employeeDao;
 
     @Test
     public void testSaveManyToMany(){
@@ -21,6 +25,7 @@ public class CompanyDaoTestSuite {
         Employee johnSmith = new Employee("John", "Smith");
         Employee stephanieClarckson = new Employee("Stephanie", "Clarckson");
         Employee lindaKovalsky = new Employee("Linda", "Kovalsky");
+        Employee mirandaKovalsky = new Employee("Miranda", "Kovalsky");
 
         Company softwareMachine = new Company("Software Machine");
         Company dataMaesters = new Company("Data Maesters");
@@ -31,12 +36,14 @@ public class CompanyDaoTestSuite {
         dataMaesters.getEmployees().add(lindaKovalsky);
         greyMatter.getEmployees().add(johnSmith);
         greyMatter.getEmployees().add(lindaKovalsky);
+        greyMatter.getEmployees().add(mirandaKovalsky);
 
         johnSmith.getCompanies().add(softwareMachine);
         johnSmith.getCompanies().add(greyMatter);
         stephanieClarckson.getCompanies().add(dataMaesters);
         lindaKovalsky.getCompanies().add(dataMaesters);
         lindaKovalsky.getCompanies().add(greyMatter);
+        mirandaKovalsky.getCompanies().add(greyMatter);
 
         //When
         companyDao.save(softwareMachine);
@@ -53,11 +60,77 @@ public class CompanyDaoTestSuite {
 
         //CleanUp
         try {
+            System.out.println("Clean up started.");
             companyDao.delete(softwareMachineId);
             companyDao.delete(dataMaestersId);
             companyDao.delete(greyMatterId);
+            System.out.println("Clean up finished.");
         } catch (Exception e) {
-            //do nothing
+            System.out.println("Exception during clean up occur: " + e);
+        }
+    }
+
+    @Test
+    public void testNamedQueries() {
+        //Given
+        Employee johnSmith = new Employee("John", "Smith");
+        Employee stephanieClarckson = new Employee("Stephanie", "Clarckson");
+        Employee lindaKovalsky = new Employee("Linda", "Kovalsky");
+        Employee mirandaKovalsky = new Employee("Miranda", "Kovalsky");
+
+        Company softwareMachine = new Company("Software Machine");
+        Company dataMasters = new Company("Data Masters");
+        //Company greyMatter = new Company("Grey Matter");
+        Company greyLife = new Company("Grey Life");
+
+        softwareMachine.getEmployees().add(johnSmith);
+        dataMasters.getEmployees().add(stephanieClarckson);
+        dataMasters.getEmployees().add(lindaKovalsky);
+        //greyMatter.getEmployees().add(johnSmith);
+        //greyMatter.getEmployees().add(lindaKovalsky);
+        greyLife.getEmployees().add(mirandaKovalsky);
+
+        johnSmith.getCompanies().add(softwareMachine);
+        //johnSmith.getCompanies().add(greyMatter);
+        stephanieClarckson.getCompanies().add(dataMasters);
+        lindaKovalsky.getCompanies().add(dataMasters);
+        //lindaKovalsky.getCompanies().add(greyMatter);
+        mirandaKovalsky.getCompanies().add(greyLife);
+
+        //When
+        companyDao.save(softwareMachine);
+        int softwareMachineId = softwareMachine.getId();
+        System.out.println("softwareMachineId = " + softwareMachineId);
+        companyDao.save(dataMasters);
+        int dataMaestersId = dataMasters.getId();
+        System.out.println("dataMaestersId = " + dataMaestersId);
+        //companyDao.save(greyMatter);
+        //int greyMatterId = greyMatter.getId();
+        //System.out.println("greyMatterId = " + greyMatterId);
+        companyDao.save(greyLife);
+        int greyLifeId = greyLife.getId();
+        System.out.println("greyLifeId = " + greyLifeId);
+
+        List<Employee> allEmployeesWithNameKovalsky = employeeDao.retrieveEmployeesByLastName("Kovalsky");
+        List<Company> companiesWithNamesStartingFor = companyDao.retrieveCompaniesWithNamesStartingFor("Grey");
+
+        //Then
+        Assert.assertEquals(2, allEmployeesWithNameKovalsky.size());    // Linda and Miranda Kovalsky
+        Assert.assertEquals(1, companiesWithNamesStartingFor.size());
+
+        /*Assert.assertEquals(2, allEmployeesWithNameKovalsky.size());    // Linda and Miranda Kovalsky
+        Assert.assertEquals(2, companiesWithNamesStartingFor.size());*/
+
+        //CleanUp
+        try {
+            System.out.println("Clean up started.");
+            companyDao.delete(softwareMachineId);
+            companyDao.delete(dataMaestersId);
+            //companyDao.delete(greyMatterId);
+            companyDao.delete(greyLifeId);
+            System.out.println("Clean up finished.");
+        } catch (Exception e) {
+            System.out.println("Exception during clean up occur: " + e);
         }
     }
 }

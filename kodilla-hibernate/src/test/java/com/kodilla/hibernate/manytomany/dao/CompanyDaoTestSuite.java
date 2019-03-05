2fbe950b3e385806545d6d2085829cb1857cdff1
ArchitectOfCode <zbigniew_g@o2.pl>
2019-03-05 14:02:41 +0100

    Minor cleanings

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
@@ -28,34 +28,42 @@ public class CompanyDaoTestSuite {
         Employee mirandaKovalsky = new Employee("Miranda", "Kovalsky");
 
         Company softwareMachine = new Company("Software Machine");
-        Company dataMaesters = new Company("Data Maesters");
+        Company dataMasters = new Company("Data Masters");
         Company greyMatter = new Company("Grey Matter");
 
         softwareMachine.getEmployees().add(johnSmith);
-        dataMaesters.getEmployees().add(stephanieClarckson);
-        dataMaesters.getEmployees().add(lindaKovalsky);
+        dataMasters.getEmployees().add(stephanieClarckson);
+        dataMasters.getEmployees().add(lindaKovalsky);
         greyMatter.getEmployees().add(johnSmith);
         greyMatter.getEmployees().add(lindaKovalsky);
         greyMatter.getEmployees().add(mirandaKovalsky);
 
+        johnSmith.getCompanies().add(softwareMachine);
+        johnSmith.getCompanies().add(greyMatter);
+        stephanieClarckson.getCompanies().add(dataMasters);
+        lindaKovalsky.getCompanies().add(dataMasters);
+        lindaKovalsky.getCompanies().add(greyMatter);
+        mirandaKovalsky.getCompanies().add(greyMatter);
+
         //When
         companyDao.save(softwareMachine);
         int softwareMachineId = softwareMachine.getId();
-        companyDao.save(dataMaesters);
-        int dataMaestersId = dataMaesters.getId();
+        companyDao.save(dataMasters);
+        int dataMastersId = dataMasters.getId();
         companyDao.save(greyMatter);
         int greyMatterId = greyMatter.getId();
 
         //Then
         Assert.assertNotEquals(0, softwareMachineId);
-        Assert.assertNotEquals(0, dataMaestersId);
+        Assert.assertNotEquals(0, dataMastersId);
         Assert.assertNotEquals(0, greyMatterId);
 
         //CleanUp
         try {
             System.out.println("Clean up started.");
+            //companyDao.deleteAll();
             companyDao.delete(softwareMachineId);
-            companyDao.delete(dataMaestersId);
+            companyDao.delete(dataMastersId);
             companyDao.delete(greyMatterId);
             System.out.println("Clean up finished.");
         } catch (Exception e) {
@@ -83,13 +91,20 @@ public class CompanyDaoTestSuite {
         greyMatter.getEmployees().add(lindaKovalsky);
         greyLife.getEmployees().add(mirandaKovalsky);
 
+        johnSmith.getCompanies().add(softwareMachine);
+        johnSmith.getCompanies().add(greyMatter);
+        stephanieClarckson.getCompanies().add(dataMasters);
+        lindaKovalsky.getCompanies().add(dataMasters);
+        lindaKovalsky.getCompanies().add(greyMatter);
+        mirandaKovalsky.getCompanies().add(greyLife);
+
         //When
         companyDao.save(softwareMachine);
         int softwareMachineId = softwareMachine.getId();
         System.out.println("softwareMachineId = " + softwareMachineId);
         companyDao.save(dataMasters);
-        int dataMaestersId = dataMasters.getId();
-        System.out.println("dataMaestersId = " + dataMaestersId);
+        int dataMastersId = dataMasters.getId();
+        System.out.println("dataMaestersId = " + dataMastersId);
         companyDao.save(greyMatter);
         int greyMatterId = greyMatter.getId();
         System.out.println("greyMatterId = " + greyMatterId);
@@ -101,17 +116,18 @@ public class CompanyDaoTestSuite {
         List<Company> companiesWithNamesStartingFor = companyDao.retrieveCompaniesWithNamesStartingFor("Grey");
 
         //Then
-        Assert.assertEquals(1, allEmployeesWithNameKovalsky.size());    // Linda and Miranda Kovalsky
-        Assert.assertEquals(1, companiesWithNamesStartingFor.size());
-
-        Assert.assertEquals(2, allEmployeesWithNameKovalsky.size());    // Linda and Miranda Kovalsky
+        Assert.assertEquals(2, allEmployeesWithNameKovalsky.size());
         Assert.assertEquals(2, companiesWithNamesStartingFor.size());
 
         //CleanUp
         try {
             System.out.println("Clean up started.");
+            employeeDao.delete(johnSmith);
+            employeeDao.delete(stephanieClarckson);
+            employeeDao.delete(lindaKovalsky);
+            employeeDao.delete(mirandaKovalsky);
             companyDao.delete(softwareMachineId);
-            companyDao.delete(dataMaestersId);
+            companyDao.delete(dataMastersId);
             companyDao.delete(greyMatterId);
             companyDao.delete(greyLifeId);
             System.out.println("Clean up finished.");
